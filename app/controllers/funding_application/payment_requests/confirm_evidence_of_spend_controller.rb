bc7011bc1aa13ed576a5187e28dd2512edcde3d2
Stuart McColl <7103229+stuartmccoll@users.noreply.github.com>
2021-02-12 10:29:59 +0000

    Refactored confirm evidence of spend page to account for all costs
    This commit refactors the evidence of spend page to account for all
    possible costs to display - those which a grantee has evidenced,
    as well as those retrieved from Salesforce which have yet to be
    evidenced.

--- app/controllers/funding_application/payment_requests/confirm_evidence_of_spend_controller.rb
+++ app/controllers/funding_application/payment_requests/confirm_evidence_of_spend_controller.rb
@@ -9,6 +9,15 @@ class FundingApplication::PaymentRequests::ConfirmEvidenceOfSpendController < Ap
     @agreed_project_costs = retrieve_agreed_project_costs(@funding_application)
     @agreed_project_costs_total = calculate_agreed_project_costs_total(@agreed_project_costs)
 
+    # It's possible that we've retrieved agreed costs from Salesforce that a user
+    # has yet to evidence with spend items
+    @agreed_project_costs_not_yet_evidenced = @agreed_project_costs.select { |apc|
+      !CostType.find(
+        @payment_request.spends.pluck(:cost_type_id).uniq
+      ).pluck(:name)
+        .include?(apc.Cost_heading__c)
+    }
+
   end
 
   def update
