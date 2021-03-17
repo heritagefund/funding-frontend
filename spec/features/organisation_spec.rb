fba232b3a4fd9fdc4f04b3534d52bcb1011115dd
Stuart McColl <7103229+stuartmccoll@users.noreply.github.com>
2021-03-17 08:28:46 +0000

    Updated organisation feature tests to account for I18n
    This commit updates the existing organisation feature tests to utilise relevant I18n translations rather than hardcoded strings of text. As well as checking for page content, we're now also checking for page titles, making these tests slightly more stringent.

--- spec/features/organisation_spec.rb
+++ spec/features/organisation_spec.rb
@@ -3,65 +3,77 @@ require 'rails_helper'
 RSpec.feature 'Organisation', type: :feature do
 
   scenario 'Successful creation of an organisation with a single legal ' \
-           'signatory' do
+    'signatory' do
 
     begin
 
       Flipper[:new_applications_enabled].enable
 
       user = FactoryBot.create(
-          :user,
-          name: 'Jane Doe',
-          date_of_birth: Date.new,
-          line1: 'line 1',
-          phone_number: '123',
-          postcode: 'W11AA',
-          townCity: 'London',
-          county: 'London'
+        :user,
+        name: 'Jane Doe',
+        date_of_birth: Date.new,
+        line1: 'line 1',
+        phone_number: '123',
+        postcode: 'W11AA',
+        townCity: 'London',
+        county: 'London'
       )
 
       login_as(user, :scope => :user)
 
       visit '/'
-      expect(page).to have_text I18n.t("dashboard.funding_applications.buttons.start")
 
-      click_link_or_button I18n.t("dashboard.funding_applications.buttons.start")
-          
-      expect(page)
-          .to have_text 'What type of organisation will be running your project'
+      expect(page).to have_text(
+        I18n.t('dashboard.funding_applications.buttons.start')
+      )
 
-      choose 'Registered charity'
+      click_link_or_button I18n.t('dashboard.funding_applications.buttons.start')
+
+      expect(page.title).to include(I18n.t('organisation.type.page_title'))
+
+      expect(page).to have_text I18n.t('organisation.type.page_heading')
+
+      choose I18n.t('organisation.type.labels.registered_charity')
+
       click_link_or_button 'Save and continue'
 
+      expect(page.title).to include(I18n.t('organisation.numbers.page_title'))
+
       expect(page)
-          .to have_text 'What is your company number?'
+          .to have_text I18n.t('organisation.numbers.labels.company_number')
       expect(page)
-          .to have_text 'What is your charity number?'
+          .to have_text I18n.t('organisation.numbers.labels.charity_number')
 
       fill_in 'organisation[charity_number]', with: '123'
+
       click_link_or_button 'Save and continue'
 
       set_address(title_field = 'Organisation name')
 
-      expect(page)
-          .to have_text 'Tell us about the mission, or objectives, of your ' \
-                      'organisation'
+      expect(page.title).to include(I18n.t('organisation.mission.page_title'))
 
-      check 'Female led'
-      click_link_or_button 'Save and continue'
+      expect(page).to have_text I18n.t('organisation.mission.page_heading')
 
-      expect(page)
-          .to have_text 'Who is your legal signatory?'
-
-      fill_in 'Full name', match: :first, with: 'Jane Doe'
-      fill_in 'Email address', match: :first, with: 'test@example.com'
-      fill_in 'Phone number', match: :first, with: '123'
+      check I18n.t('organisation.mission.labels.female_led')
 
       click_link_or_button 'Save and continue'
 
+      expect(page.title).to include(I18n.t('organisation.signatories.page_title'))
 
-      expect(page).to have_text('Check your answers')
-      expect(page).to have_text('Registered charity')
+      expect(page).to have_text I18n.t('organisation.signatories.page_heading')
+
+      fill_in I18n.t('organisation.signatories.labels.full_name'), match: :first, with: 'Jane Doe'
+      fill_in I18n.t('organisation.signatories.labels.email_address'), match: :first, with: 'test@example.com'
+      fill_in I18n.t('organisation.signatories.labels.phone_number'), match: :first, with: '123'
+
+      click_link_or_button 'Save and continue'
+
+      expect(page.title).to include(I18n.t('organisation.summary.page_title'))
+      expect(page).to have_text(I18n.t('organisation.summary.page_heading'))
+      expect(page).to have_text(
+        I18n.t('organisation.type.labels.registered_charity')
+      )
       expect(page).to have_text('123')
       expect(page).to have_text('test')
       expect(page).to have_text('4 Barons Court Road')
@@ -80,10 +92,8 @@ RSpec.feature 'Organisation', type: :feature do
       expect(organisation.postcode).to eq('W14 9DT')
       expect(organisation.mission).to include('female_led')
       expect(organisation.legal_signatories.first.name).to eq('Jane Doe')
-      expect(organisation.legal_signatories.first.email_address)
-          .to eq('test@example.com')
-      expect(organisation.legal_signatories.first.phone_number)
-          .to eq('123')
+      expect(organisation.legal_signatories.first.email_address).to eq('test@example.com')
+      expect(organisation.legal_signatories.first.phone_number).to eq('123')
 
     ensure
 
@@ -100,31 +110,34 @@ RSpec.feature 'Organisation', type: :feature do
       Flipper[:new_applications_enabled].enable
 
       user = FactoryBot.create(
-          :user,
-          name: 'Jane Doe',
-          date_of_birth: Date.new,
-          line1: 'line 1',
-          phone_number: '123',
-          postcode: 'W11AA',
-          townCity: 'London',
-          county: 'London'
+        :user,
+        name: 'Jane Doe',
+        date_of_birth: Date.new,
+        line1: 'line 1',
+        phone_number: '123',
+        postcode: 'W11AA',
+        townCity: 'London',
+        county: 'London'
       )
 
       login_as(user, :scope => :user)
 
       visit '/'
-      expect(page).to have_text 'Start a new application'
+      expect(page).to have_text(
+        I18n.t('dashboard.funding_applications.buttons.start')
+      )
 
-      click_link_or_button 'Start a new application'
+      click_link_or_button I18n.t('dashboard.funding_applications.buttons.start')
 
-      expect(page)
-          .to have_text 'What type of organisation will be running your project'
+      expect(page.title).to include(I18n.t('organisation.type.page_title'))
+
+      expect(page).to have_text I18n.t('organisation.type.page_heading')
 
       click_link_or_button 'Save and continue'
 
-      expect(page)
-          .to have_text 'Error: Select the type of organisation that will be ' \
-                      'running your project'
+      expect(page).to have_text(
+        "#{I18n.t('generic.error')}: #{I18n.t('activerecord.errors.models.organisation.attributes.org_type.blank')}"
+      )
 
     ensure
 
@@ -135,21 +148,26 @@ RSpec.feature 'Organisation', type: :feature do
   end
 
   scenario 'New Applications Enabled Flipper turned off should not show Start a new project button' do
+
     user = FactoryBot.create(
-        :user,
-        name: 'Jane Doe',
-        date_of_birth: Date.new,
-        line1: 'line 1',
-        phone_number: '123',
-        postcode: 'W11AA',
-        townCity: 'London',
-        county: 'London'
+      :user,
+      name: 'Jane Doe',
+      date_of_birth: Date.new,
+      line1: 'line 1',
+      phone_number: '123',
+      postcode: 'W11AA',
+      townCity: 'London',
+      county: 'London'
     )
 
     login_as(user, :scope => :user)
 
     visit '/'
-    expect(page).not_to have_text 'Start a new project'
+
+    expect(page).not_to have_text(
+      I18n.t('dashboard.funding_applications.buttons.start')
+    )
+
   end
 
 end
